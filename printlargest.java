f256229f0e785e3dbedbd354c92a19d3efb7fdaa
Uma Mina <62588358+umamina11@users.noreply.github.com>
2024-01-18 10:42:55 +0530

    Three Largest numbers in Array
    This code presents the three largest numbers/elements in the given array

---
+++ printlargest.java
@@ -0,0 +1,49 @@
+//Write a code to find the largest three elements.
+// Time complexity will be O(n) and extra space is O(1)
+
+class PrintLargest {
+
+    static void print3largest(int arr[], int arr_size) {
+        int i, first, second, third;
+
+        /* Size of the array must be greater than three */
+        if (arr_size < 3) {
+            System.out.print(" Need more elements ");
+            return;
+        }
+
+        third = first = second = Integer.MIN_VALUE;
+        for (i = 0; i < arr_size; i++) {
+            /*
+             * If current element is greater than
+             * first
+             */
+            if (arr[i] > first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            }
+
+            /*
+             * If arr[i] is in between first and
+             * second then update second
+             */
+            else if (arr[i] > second) {
+                third = second;
+                second = arr[i];
+            }
+
+            else if (arr[i] > third)
+                third = arr[i];
+        }
+
+        System.out.println("Three largest elements are " + first + " " + second + " " + third);
+    }
+
+    /* Providing the array */
+    public static void main(String[] args) {
+        int arr[] = { 11, 24, 46, 71, 11, 1 };
+        int n = arr.length;
+        print3largest(arr, n);
+    }
+}
