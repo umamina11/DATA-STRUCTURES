213f14bf866d9fb5c54b5b77c8d25b069360d298
Uma Mina <62588358+umamina11@users.noreply.github.com>
2024-03-23 16:39:22 +0530

    sorting using predefined methods in jdk

---
+++ sortingusingsortmethods.java
@@ -0,0 +1,13 @@
+public class sortingusingsortmethods {
+    public static void main(String[] args) {
+        int[] arr = { 20, 35, -15, 7, 55, 1, -22 };
+
+        Arrays.sort(arr);
+        // calling parallel sort
+        Arrays.parallelSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
