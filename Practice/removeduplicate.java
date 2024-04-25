24cb54dfe89aa87bb4287ec9d90a3f918b6e131c
Uma Mina <62588358+umamina11@users.noreply.github.com>
2024-04-25 22:35:20 +0530

    Update removeduplicate.java

---
+++ Practice/removeduplicate.java
@@ -0,0 +1,60 @@
+/*Remove Duplicates from Sorted List
+You are given head of the non-empty sorted linked list where the value of the 
+𝑖
+𝑡
+ℎ
+i 
+th
+  node will be 
+𝐴
+𝑖
+A 
+i
+​
+ .
+
+Your task is to delete all duplicates such that each element appears only once and return the linked list sorted.
+
+Input:
+First line will contain 
+𝑇
+T, number of test cases. Then the test cases follow.
+The first line contains one integer 
+𝑁
+N — the length of the linked list.
+The second line contains 
+𝑁
+N space separated integers 
+𝐴
+1
+,
+𝐴
+2
+,
+…
+𝐴
+𝑁
+A 
+1
+​
+ ,A 
+2
+​
+ ,…A 
+N
+​
+  — the value of the linked list nodes starting from the head for the linked list.
+Note:
+
+For Java language, you need to:
+Complete the function in the submit solution tab:
+
+Node removeDuplicates(Node head){...}
+ 
+  */
+
+package Practice;
+
+public class removeduplicate {
+    
+}
